57b149870050764203718ad1bc7385ea3f1dcaf4
john manuvel <john@thunai.ai>
2025-12-10 20:55:58 +0530

    fix

--- src/environment.ts
+++ src/environment.ts
@@ -3,11 +3,11 @@
 export const environment = {
     production: false,
     baseUrl: 'https://nag-dev.infisign.net',//SSO URL
-    redirectUri: 'https://eastern-bank-application-page.vercel.app/login/oidc/callback',
-    tenantId: 'infisign0446438',
-    clientId: '430290',
-    clientSecret: '3ebfe20b2a9e2ba57fe224f4be1f8161bb8990386e03ac02cfcb406a',
-    urlIdentifier: 'intuitive',
-    accessTokenApiUrl: 'https://nag-dev.infisign.net/openid-service/openid/infisign0446438/token',
-    userInfoApiUrl: 'https://nag-dev.infisign.net/openid-service/openid/infisign0446438/userinfo'
+    redirectUri: 'https://easternbank-liart.vercel.app/login/oidc/callback',
+    tenantId: 'infisign8647640',
+    clientId: '862660',
+    clientSecret: '72d4d453ece5788b3aa67fcd353671d9b56de15c70ee70adbb987287',
+    urlIdentifier: 'easternbank',
+    accessTokenApiUrl: 'https://nag-dev.infisign.net/openid-service/openid/infisign8647640/token',
+    userInfoApiUrl: 'https://nag-dev.infisign.net/openid-service/openid/infisign8647640/userinfo'
 };
